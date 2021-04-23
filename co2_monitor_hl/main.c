e5a386c19aac56d4f8e3d817fe86f874526081b1
Dave Glover <dglover.au@hotmail.com>
2021-04-23 22:07:37 +1000

    Updated to the DevX library

--- co2_monitor_hl/main.c
+++ co2_monitor_hl/main.c
@@ -7,7 +7,7 @@
  *   The learning_path_libs functions provided in the learning_path_libs folder:
  *
  *	   1. are NOT supported Azure Sphere APIs.
- *	   2. are prefixed with lp_, typedefs are prefixed with LP_
+ *	   2. are prefixed with dx_, typedefs are prefixed with DX_
  *	   3. are built from the Azure Sphere SDK Samples at https://github.com/Azure/azure-sphere-samples
  *	   4. are not intended as a substitute for understanding the Azure Sphere SDK Samples.
  *	   5. aim to follow best practices as demonstrated by the Azure Sphere SDK Samples.
@@ -17,13 +17,13 @@
 
 #include "hw/azure_sphere_learning_path.h"
 
-#include "azure_iot.h"
-#include "exit_codes.h"
-#include "config.h"
-#include "inter_core.h"
-#include "peripheral_gpio.h"
-#include "terminate.h"
-#include "timer.h"
+#include "dx_azure_iot.h"
+#include "dx_config.h"
+#include "dx_exit_codes.h"
+#include "dx_gpio.h"
+#include "dx_intercore.h"
+#include "dx_terminate.h"
+#include "dx_timer.h"
 
 #include "applibs_versions.h"
 #include <applibs/gpio.h>
@@ -38,16 +38,18 @@
 
 
 #define JSON_MESSAGE_BYTES 256 // Number of bytes to allocate for the JSON telemetry message for IoT Central
+#define OneMS 1000000		// used to simplify timer defn.
 
  // Forward signatures
+static void CO2AlertBuzzerOffOneShotTimer(EventLoopTimer* eventLoopTimer);
+static void CO2AlertHandler(EventLoopTimer* eventLoopTimer);
+static void DeviceTwinGenericHandler(DX_DEVICE_TWIN_BINDING* deviceTwinBinding);
+static void FlashLedOffTimerHandler(EventLoopTimer* eventLoopTimer);
+static void FlashLEDsTimerHandler(EventLoopTimer* eventLoopTimer);
 static void MeasureSensorHandler(EventLoopTimer* eventLoopTimer);
 static void PublishTelemetryTimer(EventLoopTimer* eventLoopTimer);
-static void NetworkConnectionStatusHandler(EventLoopTimer* eventLoopTimer);
-static void CO2AlertHandler(EventLoopTimer* eventLoopTimer);
-static void CO2AlertBuzzerOffOneShotTimer(EventLoopTimer* eventLoopTimer);
-static void DeviceTwinGenericHandler(LP_DEVICE_TWIN_BINDING* deviceTwinBinding);
 
-LP_USER_CONFIG lp_config;
+DX_USER_CONFIG dx_config;
 
 static char msgBuffer[JSON_MESSAGE_BYTES] = { 0 };
 
@@ -56,67 +58,88 @@ static const struct timespec co2AlertBuzzerPeriod = { 0, 5 * 100 * 1000 };
 
 // GPIO Output PeripheralGpios
 #ifdef OEM_SEEED_STUDIO
-static LP_GPIO co2AlertPin = { .pin = CO2_ALERT, .direction = LP_OUTPUT, .initialState = GPIO_Value_Low, .name = "co2AlertPin" };
+static DX_GPIO co2AlertPin = { .pin = CO2_ALERT, .direction = DX_OUTPUT, .initialState = GPIO_Value_Low, .name = "co2AlertPin" };
 #endif // OEM_AVNET
 
 #ifdef OEM_AVNET
-static LP_PERIPHERAL_GPIO hvacHeatingLed = { .pin = LED_RED, .direction = LP_OUTPUT, .initialState = GPIO_Value_Low, .invertPin = true, .initialise = lp_openPeripheralGpio, .name = "hvacHeatingLed" };
-static LP_PERIPHERAL_GPIO hvacCoolingLed = { .pin = LED_BLUE, .direction = LP_OUTPUT, .initialState = GPIO_Value_Low, .invertPin = true, .initialise = lp_openPeripheralGpio, .name = "hvacCoolingLed" };
-static LP_PERIPHERAL_GPIO co2AlertPin = { .pin = CO2_ALERT, .direction = LP_OUTPUT, .initialState = GPIO_Value_Low, .invertPin = true, .initialise = lp_openPeripheralGpio, .name = "co2AlertPin" };
+static DX_PERIPHERAL_GPIO hvacHeatingLed = { .pin = LED_RED, .direction = DX_OUTPUT, .initialState = GPIO_Value_Low, .invertPin = true, .initialise = dx_openPeripheralGpio, .name = "hvacHeatingLed" };
+static DX_PERIPHERAL_GPIO hvacCoolingLed = { .pin = LED_BLUE, .direction = DX_OUTPUT, .initialState = GPIO_Value_Low, .invertPin = true, .initialise = dx_openPeripheralGpio, .name = "hvacCoolingLed" };
+static DX_PERIPHERAL_GPIO co2AlertPin = { .pin = CO2_ALERT, .direction = DX_OUTPUT, .initialState = GPIO_Value_Low, .invertPin = true, .initialise = dx_openPeripheralGpio, .name = "co2AlertPin" };
 #endif // OEM_AVNET
 
-static LP_GPIO azureIotConnectedLed = { .pin = NETWORK_CONNECTED_LED, .direction = LP_OUTPUT, .initialState = GPIO_Value_Low, .invertPin = true, .name = "azureConnectedLed" };
+static DX_GPIO azureIotConnectedLed = { .pin = NETWORK_CONNECTED_LED, .direction = DX_OUTPUT, .initialState = GPIO_Value_Low, .invertPin = true, .name = "azureConnectedLed" };
 
 // Timers
-static LP_TIMER azureConnectedStatusTimer = { .period = {10, 0}, .name = "azureConnectedStatusTimer", .handler = NetworkConnectionStatusHandler };
-static LP_TIMER measureSensorTimer = { .period = {20, 0}, .name = "measureSensorTimer", .handler = MeasureSensorHandler };
-static LP_TIMER publishTelemetryTimer = { .period = {30, 0}, .name = "publishTelemetryTimer", .handler = PublishTelemetryTimer };
-static LP_TIMER co2AlertTimer = { .period = {4, 0}, .name = "co2AlertTimer", .handler = CO2AlertHandler };
-static LP_TIMER co2AlertBuzzerOffOneShotTimer = { .period = { 0, 0 }, .name = "co2AlertBuzzerOffOneShotTimer", .handler = CO2AlertBuzzerOffOneShotTimer };
+static DX_TIMER flashLEDsTimer = { .period = {0, 0},.name = "flashLEDsTimer",.handler = FlashLEDsTimerHandler };
+static DX_TIMER flashLedOffTimer = { .period = {0,0},.name = "flashLedOffTimer",.handler = FlashLedOffTimerHandler };
+static DX_TIMER measureSensorTimer = { .period = {20, 0}, .name = "measureSensorTimer", .handler = MeasureSensorHandler };
+static DX_TIMER publishTelemetryTimer = { .period = {30, 0}, .name = "publishTelemetryTimer", .handler = PublishTelemetryTimer };
+static DX_TIMER co2AlertTimer = { .period = {4, 0}, .name = "co2AlertTimer", .handler = CO2AlertHandler };
+static DX_TIMER co2AlertBuzzerOffOneShotTimer = { .period = { 0, 0 }, .name = "co2AlertBuzzerOffOneShotTimer", .handler = CO2AlertBuzzerOffOneShotTimer };
 
 // Azure IoT Device Twins
-static LP_DEVICE_TWIN_BINDING desiredCO2AlertLevel = { .twinProperty = "DesiredCO2AlertLevel", .twinType = LP_TYPE_FLOAT, .handler = DeviceTwinGenericHandler };
-static LP_DEVICE_TWIN_BINDING actualCO2Level = { .twinProperty = "ActualCO2Level", .twinType = LP_TYPE_FLOAT };
+static DX_DEVICE_TWIN_BINDING desiredCO2AlertLevel = { .twinProperty = "DesiredCO2AlertLevel", .twinType = DX_TYPE_FLOAT, .handler = DeviceTwinGenericHandler };
+static DX_DEVICE_TWIN_BINDING actualCO2Level = { .twinProperty = "ActualCO2Level", .twinType = DX_TYPE_FLOAT };
 
 // Initialize Sets
-LP_GPIO* PeripheralGpioSet[] = { &co2AlertPin, &azureIotConnectedLed };
-LP_DEVICE_TWIN_BINDING* deviceTwinBindingSet[] = { &desiredCO2AlertLevel, &actualCO2Level };
-LP_TIMER* timerSet[] = {
-		&azureConnectedStatusTimer, &measureSensorTimer, &publishTelemetryTimer,
+DX_GPIO* PeripheralGpioSet[] = { &co2AlertPin, &azureIotConnectedLed };
+DX_DEVICE_TWIN_BINDING* deviceTwinBindingSet[] = { &desiredCO2AlertLevel, &actualCO2Level };
+DX_TIMER* timerSet[] = {
+		&flashLEDsTimer, & flashLedOffTimer, &measureSensorTimer, &publishTelemetryTimer,
 		&co2AlertTimer, &co2AlertBuzzerOffOneShotTimer
 };
 
 static const char* MsgTemplate = "{ \"CO2\": %3.2f, \"Temperature\": %3.2f, \"Humidity\": \"%3.1f\", \"MsgId\":%d }";
-static LP_MESSAGE_PROPERTY* telemetryMessageProperties[] = {
-	&(LP_MESSAGE_PROPERTY) { .key = "appid", .value = "co2monitor" },
-	&(LP_MESSAGE_PROPERTY) {.key = "format", .value = "json" },
-	&(LP_MESSAGE_PROPERTY) {.key = "type", .value = "telemetry" },
-	&(LP_MESSAGE_PROPERTY) {.key = "version", .value = "1" }
+static DX_MESSAGE_PROPERTY* telemetryMessageProperties[] = {
+	&(DX_MESSAGE_PROPERTY) { .key = "appid", .value = "co2monitor" },
+	&(DX_MESSAGE_PROPERTY) {.key = "format", .value = "json" },
+	&(DX_MESSAGE_PROPERTY) {.key = "type", .value = "telemetry" },
+	&(DX_MESSAGE_PROPERTY) {.key = "version", .value = "1" }
 };
+
+static void FlashLedOffTimerHandler(EventLoopTimer* eventLoopTimer) {
+	if (ConsumeEventLoopTimerEvent(eventLoopTimer) != 0) {
+		dx_terminate(DX_ExitCode_ConsumeEventLoopTimeEvent);
+		return;
+	}
+	dx_gpioOff(&azureIotConnectedLed);
+}
+
 
 /// <summary>
 /// Check status of connection to Azure IoT
 /// </summary>
-static void NetworkConnectionStatusHandler(EventLoopTimer* eventLoopTimer)
+static void FlashLEDsTimerHandler(EventLoopTimer* eventLoopTimer)
 {
 	static bool toggleConnectionStatusLed = true;
 
 	if (ConsumeEventLoopTimerEvent(eventLoopTimer) != 0)
 	{
-		lp_terminate(ExitCode_ConsumeEventLoopTimeEvent);
+		dx_terminate(DX_ExitCode_ConsumeEventLoopTimeEvent);
+		return;
 	}
-	else
-	{
-		if (lp_azureConnect())
-		{
-			lp_gpioStateSet(&azureIotConnectedLed, toggleConnectionStatusLed);
-			toggleConnectionStatusLed = !toggleConnectionStatusLed;
-		}
-		else
-		{
-			lp_gpioStateSet(&azureIotConnectedLed, false);
-		}
+	if (dx_azureIsConnected()) {
+
+		dx_gpioOn(&azureIotConnectedLed);
+		// on for 1300ms off for 100ms = 1400 ms in total
+		dx_timerOneShotSet(&flashLEDsTimer, &(struct timespec){1, 400 * OneMS});
+		dx_timerOneShotSet(&flashLedOffTimer, &(struct timespec){1, 300 * OneMS});
+
+	} else if (dx_isNetworkReady()) {
+
+		dx_gpioOn(&azureIotConnectedLed);
+		// on for 100ms off for 1300ms = 1400 ms in total
+		dx_timerOneShotSet(&flashLEDsTimer, &(struct timespec){1, 400 * OneMS});
+		dx_timerOneShotSet(&flashLedOffTimer, &(struct timespec){0, 100 * OneMS});
+
+	} else {
+
+		dx_gpioOn(&azureIotConnectedLed);
+		// on for 700ms off for 700ms = 1400 ms in total
+		dx_timerOneShotSet(&flashLEDsTimer, &(struct timespec){1, 400 * OneMS});
+		dx_timerOneShotSet(&flashLedOffTimer, &(struct timespec){0, 700 * OneMS});
 	}
+
 }
 
 
@@ -127,10 +150,10 @@ static void CO2AlertBuzzerOffOneShotTimer(EventLoopTimer* eventLoopTimer)
 {
 	if (ConsumeEventLoopTimerEvent(eventLoopTimer) != 0)
 	{
-		lp_terminate(ExitCode_ConsumeEventLoopTimeEvent);
+		dx_terminate(DX_ExitCode_ConsumeEventLoopTimeEvent);
 		return;
 	}
-	lp_gpioOff(&co2AlertPin);
+	dx_gpioOff(&co2AlertPin);
 }
 
 
@@ -143,14 +166,14 @@ static void CO2AlertHandler(EventLoopTimer* eventLoopTimer)
 
 	if (ConsumeEventLoopTimerEvent(eventLoopTimer) != 0)
 	{
-		lp_terminate(ExitCode_ConsumeEventLoopTimeEvent);
+		dx_terminate(DX_ExitCode_ConsumeEventLoopTimeEvent);
 	}
 	else
 	{
 		if (desiredCO2AlertLevel.twinStateUpdated && !isnan(co2_ppm) && co2_ppm > * (float*)desiredCO2AlertLevel.twinState)
 		{
-			lp_gpioOn(&co2AlertPin);
-			lp_timerOneShotSet(&co2AlertBuzzerOffOneShotTimer, &co2AlertBuzzerPeriod);
+			dx_gpioOn(&co2AlertPin);
+			dx_timerOneShotSet(&co2AlertBuzzerOffOneShotTimer, &co2AlertBuzzerPeriod);
 		}
 	}
 }
@@ -161,7 +184,7 @@ static void PublishTelemetryTimer(EventLoopTimer* eventLoopTimer)
 
 	if (ConsumeEventLoopTimerEvent(eventLoopTimer) != 0)
 	{
-		lp_terminate(ExitCode_ConsumeEventLoopTimeEvent);
+		dx_terminate(DX_ExitCode_ConsumeEventLoopTimeEvent);
 	}
 	else
 	{
@@ -170,9 +193,9 @@ static void PublishTelemetryTimer(EventLoopTimer* eventLoopTimer)
 			if (snprintf(msgBuffer, JSON_MESSAGE_BYTES, MsgTemplate, co2_ppm, temperature, relative_humidity, ++msgId) > 0)
 			{
 				Log_Debug("%s\n", msgBuffer);
-				lp_azureMsgSendWithProperties(msgBuffer, telemetryMessageProperties, NELEMS(telemetryMessageProperties));
+				dx_azureMsgSendWithProperties(msgBuffer, telemetryMessageProperties, NELEMS(telemetryMessageProperties));
 			}
-			lp_deviceTwinReportState(&actualCO2Level, &co2_ppm);
+			dx_deviceTwinReportState(&actualCO2Level, &co2_ppm);
 		}
 	}
 }
@@ -185,7 +208,7 @@ static void MeasureSensorHandler(EventLoopTimer* eventLoopTimer)
 {
 	if (ConsumeEventLoopTimerEvent(eventLoopTimer) != 0)
 	{
-		lp_terminate(ExitCode_ConsumeEventLoopTimeEvent);
+		dx_terminate(DX_ExitCode_ConsumeEventLoopTimeEvent);
 	}
 	else
 	{
@@ -199,10 +222,10 @@ static void MeasureSensorHandler(EventLoopTimer* eventLoopTimer)
 /// <summary>
 /// Generic Device Twin Handler. It just sets reported state for the twin
 /// </summary>
-static void DeviceTwinGenericHandler(LP_DEVICE_TWIN_BINDING* deviceTwinBinding)
+static void DeviceTwinGenericHandler(DX_DEVICE_TWIN_BINDING* deviceTwinBinding)
 {
-	lp_deviceTwinReportState(deviceTwinBinding, deviceTwinBinding->twinState);
-	lp_deviceTwinAckDesiredState(deviceTwinBinding, deviceTwinBinding->twinState, LP_DEVICE_TWIN_COMPLETED);
+	dx_deviceTwinReportState(deviceTwinBinding, deviceTwinBinding->twinState);
+	dx_deviceTwinAckDesiredState(deviceTwinBinding, deviceTwinBinding->twinState, DX_DEVICE_TWIN_COMPLETED);
 }
 
 static bool InitializeSdc30(void)
@@ -253,18 +276,18 @@ static bool InitializeSdc30(void)
 /// <returns>0 on success, or -1 on failure</returns>
 static void InitPeripheralGpiosAndHandlers(void)
 {
-	lp_azureInitialize(lp_config.scopeId, lp_config.deviceTwinModelId);
+	dx_azureInitialize(dx_config.scopeId, NULL);
 
 	InitializeSdc30();
 
-	lp_gpioSetOpen(PeripheralGpioSet, NELEMS(PeripheralGpioSet));
-	lp_deviceTwinSetOpen(deviceTwinBindingSet, NELEMS(deviceTwinBindingSet));
+	dx_gpioSetOpen(PeripheralGpioSet, NELEMS(PeripheralGpioSet));
+	dx_deviceTwinSetOpen(deviceTwinBindingSet, NELEMS(deviceTwinBindingSet));
 
-	lp_timerSetStart(timerSet, NELEMS(timerSet));
-
-	lp_azureToDeviceStart();
+	dx_timerSetStart(timerSet, NELEMS(timerSet));
 
 	scd30_read_measurement(&co2_ppm, &temperature, &relative_humidity);
+
+	dx_timerOneShotSet(&flashLEDsTimer, &(struct timespec){1, 0});
 }
 
 /// <summary>
@@ -274,43 +297,43 @@ static void ClosePeripheralGpiosAndHandlers(void)
 {
 	Log_Debug("Closing file descriptors\n");
 
-	lp_timerSetStop(timerSet, NELEMS(timerSet));
-	lp_azureToDeviceStop();
+	dx_timerSetStop(timerSet, NELEMS(timerSet));
+	dx_azureToDeviceStop();
 
-	lp_gpioSetClose(PeripheralGpioSet, NELEMS(PeripheralGpioSet));
+	dx_gpioSetClose(PeripheralGpioSet, NELEMS(PeripheralGpioSet));
 
-	lp_deviceTwinSetClose();
+	dx_deviceTwinSetClose();
 
 	scd30_stop_periodic_measurement();
 
-	lp_timerEventLoopStop();
+	dx_timerEventLoopStop();
 }
 
 int main(int argc, char* argv[])
 {
-	lp_registerTerminationHandler();
+	dx_registerTerminationHandler();
 
-	lp_configParseCmdLineArguments(argc, argv, &lp_config);
-	if (!lp_configValidate(&lp_config))
+	dx_configParseCmdLineArguments(argc, argv, &dx_config);
+	if (!dx_configValidate(&dx_config))
 	{
-		return lp_getTerminationExitCode();
+		return dx_getTerminationExitCode();
 	}
 
 	InitPeripheralGpiosAndHandlers();
 
 	// Main loop
-	while (!lp_isTerminationRequired())
+	while (!dx_isTerminationRequired())
 	{
-		int result = EventLoop_Run(lp_timerGetEventLoop(), -1, true);
+		int result = EventLoop_Run(dx_timerGetEventLoop(), -1, true);
 		// Continue if interrupted by signal, e.g. due to breakpoint being set.
 		if (result == -1 && errno != EINTR)
 		{
-			lp_terminate(ExitCode_Main_EventLoopFail);
+			dx_terminate(DX_ExitCode_Main_EventLoopFail);
 		}
 	}
 
 	ClosePeripheralGpiosAndHandlers();
 
 	Log_Debug("Application exiting.\n");
-	return lp_getTerminationExitCode();
+	return dx_getTerminationExitCode();
 }
